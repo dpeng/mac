0ef2161ed8282922756afb9571c0f7a666c69159
dpeng <df.peng@hotmail.com>
2017-04-09 21:48:31 +0800

    add terminal hello world project

---
+++ xcode/terminal/hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Peng Dongfeng on 2017/4/9.
+//  Copyright © 2017年 Nokia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
